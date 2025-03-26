6174aa937f2af92a135c23d1227f23b602bf6db5
Raiana Ladeira <raianasancho@gmail.com>
2025-03-26 16:32:47 -0300

    Fix

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  basePath: '/sinaf/site-viva-sinaf',
   images: {
     unoptimized: true
-  },    
-  assetPrefix: "/" ,
-  trailingSlash: true
+  }
 };
 
 module.exports = nextConfig;
